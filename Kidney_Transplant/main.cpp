335fb38eb8ea32c5c2c79f81384b402b117b377d
Trevor Ross <trevor.is.ross@gmail.com>
2015-11-01 21:04:20 -0600

    Initial Commit

---
+++ Kidney_Transplant/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Kidney_Transplant
+//
+//  Created by Trevor Ross on 11/1/15.
+//  Copyright © 2015 Trevor Ross. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+	// insert code here...
+	std::cout << "Hello, World!\n";
+    return 0;
+}
